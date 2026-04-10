707fb3ef7e2a252cdb68fbafc4960ed782604ff4
Joaotccopiadoras <joaogaia@tccopiadoras.com.br>
2026-04-10 10:18:20 -0300

    ajuste apptsx01

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,29 @@ import FinanceiroPage from "./pages/FinanceiroPage";
 //import AgendaPage from "./pages/AgendaPage"
 //import TecnicaPage from "./pages/TecnicaPage"
 
+const queryClient = new QueryClient();
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* Joga direto pro inicio */}
-        <Route path="/" element={<Navigate to="/interno" replace />} />
-        
-        {/* A Rota do Portal */}
-        <Route path="/interno" element={<PortalPage />} />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          {/* Joga direto pro inicio */}
+          <Route path="/" element={<Navigate to="/interno" replace />} />
+          
+          {/* A Rota do Portal */}
+          <Route path="/interno" element={<PortalPage />} />
 
-        {/* As Rotas dos Módulos*/}
-        <Route path="/financeiro" element={<FinanceiroPage />} />
-        {/* <Route path="/agenda" element={<AgendaPage />} /> */}
-        
-        <Route path="*" element={
-          <div className="flex h-screen items-center justify-center text-2xl font-bold text-gray-600">
-            Página não encontrada.
-          </div>
-        } />
-      </Routes>
-    </BrowserRouter>
+          {/* As Rotas dos Módulos*/}
+          <Route path="/financeiro" element={<FinanceiroPage />} />
+          {/* <Route path="/agenda" element={<AgendaPage />} /> */}
+          
+          <Route path="*" element={
+            <div className="flex h-screen items-center justify-center text-2xl font-bold text-gray-600">
+              Página não encontrada.
+            </div>
+          } />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
