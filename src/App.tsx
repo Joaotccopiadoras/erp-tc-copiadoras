aa279aa32819e73542cd186da8946c530d7be34b
Joaotccopiadoras <joaogaia@tccopiadoras.com.br>
2026-04-10 09:48:11 -0300

    add financeiroroute01

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import PortalPage from "./pages/PortalPage";
-//import FinanceiroPage from "./pages/FinanceiroPage";
+import FinanceiroPage from "./pages/FinanceiroPage";
 //import AgendaPage from "./pages/AgendaPage"
+//import TecnicaPage from "./pages/TecnicaPage"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Joga direto pro Portal */}
+        {/* Joga direto pro inicio */}
         <Route path="/" element={<Navigate to="/interno" replace />} />
         
         {/* A Rota do Portal */}
         <Route path="/interno" element={<PortalPage />} />
 
         {/* As Rotas dos Módulos*/}
-        {/* <Route path="/financeiro" element={<FinanceiroPage />} /> */}
+        <Route path="/financeiro" element={<FinanceiroPage />} />
         {/* <Route path="/agenda" element={<AgendaPage />} /> */}
         
         <Route path="*" element={
