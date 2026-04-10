4e977a5218ce223236494c71cb464dad40cfbf58
Joaotccopiadoras <joaogaia@tccopiadoras.com.br>
2026-04-10 10:17:52 -0300

    ajuste apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PortalPage from "./pages/PortalPage";
 import FinanceiroPage from "./pages/FinanceiroPage";
 //import AgendaPage from "./pages/AgendaPage"
